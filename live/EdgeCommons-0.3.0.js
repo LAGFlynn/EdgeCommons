0e29879811bffb73dbea815a63a843f2b6023e71
Simon Widjaja <mail@simonwidjaja.com>
2012-07-01 20:36:41 +0200

    Clean up

--- live/EdgeCommons-0.3.0.js
+++ live/EdgeCommons-0.3.0.js
@@ -1,4 +1,45 @@
-// Input 0
+/**
+ * EdgeCommons
+ * Dirty little Helpers for Adobe Edge
+ * by Simon Widjaja
+ *
+ * Additional Contributors:
+ * Timm Jansen, Johannes Boyne
+ *
+ * Copyright (c) 2012 Simon Widjaja
+ *
+ * --------------------------------------------------------------------------------------------------------------------------------------------------
+ * Released under MIT license
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * --------------------------------------------------------------------------------------------------------------------------------------------------
+ * Additional 3rd party libraries are used. For related credits and license models take a closer look at the affected library.
+ * --------------------------------------------------------------------------------------------------------------------------------------------------
+ * EdgeCommons uses:
+ * - Adobe Edge API
+ * - Modulog
+ * - YepNope
+ * - SoundJS (CreateJS)
+ * --------------------------------------------------------------------------------------------------------------------------------------------------
+ * Features:
+ * #Core
+ * - Comfortable Loggin and Config with Modulog
+ * - Loading external Scripts and Style Sheets with YepNope
+ * - Injecting Data
+ * #Sound
+ * - Load and play sounds with SoundJS (CreateJS)
+ * #Preload
+ * - PreloadJS (uses by SoundJS) (CreateJS)
+ */// Input 0
 (function(a){var c=function(){};c.VERSION="0.0.1";a.Modulog=c})(window);
 (function(){var a=function(){};a.VERSION="0.0.1";a.LEVEL_NONE=0;a.LEVEL_ERROR=1;a.LEVEL_WARN=2;a.LEVEL_INFO=3;a.LEVEL_DEBUG=4;a.level=a.LEVEL_DEBUG;var c=null;a.addLogTarget=function(b){"function"===typeof b&&(c=b)};a.debug=function(b,a,d){ModulogLog.level>=ModulogLog.LEVEL_DEBUG&&(b="[ DEBUG "+(a?"| "+a+" ":"")+"] "+b,"undefined"!=typeof console&&"undefined"!=typeof console.log&&(d?console.debug(b,d):console.debug(b)),ModulogLog.__delegate(b,d))};a.info=function(b,a,d){ModulogLog.level>=ModulogLog.LEVEL_INFO&&
 (b="[ INFO "+(a?"| "+a+" ":"")+"] "+b,"undefined"!=typeof console&&"undefined"!=typeof console.log&&(d?console.info(b,d):console.info(b)),ModulogLog.__delegate(b,d))};a.warn=function(b,a,d){ModulogLog.level>=ModulogLog.LEVEL_WARN&&(b="[ WARN "+(a?"| "+a+" ":"")+"] "+b,"undefined"!=typeof console&&"undefined"!=typeof console.log&&(d?console.warn(b,d):console.warn(b)),ModulogLog.__delegate(b,d))};a.error=function(a,g,d){ModulogLog.level>=ModulogLog.LEVEL_ERROR&&(a="[ ERROR "+(g?"| "+g+" ":"")+"] "+
